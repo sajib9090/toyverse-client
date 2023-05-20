c9f919d4c8b81a652bb131a56e6c3409f08364bb
sajib9090 <nispap.devil.sajib@gmail.com>
2023-05-20 10:41:11 +0600

    added some style

--- src/pages/ToyDetails/ToyDetails.jsx
+++ src/pages/ToyDetails/ToyDetails.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
+import { Rating } from '@smastrom/react-rating'
+import '@smastrom/react-rating/style.css'
 
 const ToyDetails = () => {
   const details = useLoaderData();
+  const [rating, setRating] = useState(details.rating) 
   return (
     <div>
       <div className="card lg:card-side bg-base-100 shadow-xl">
@@ -17,7 +20,7 @@ const ToyDetails = () => {
           <p>{details.sellerName}</p>
           <p>{details.sellerEmail}</p>
           <p>{details.price}</p>
-          <p>{details.rating}</p>
+          <p><Rating style={{ maxWidth: 150 }} value={rating} />{details.rating}</p>
           <p>{details.quantity}</p>
           <p>{details.description}</p>
           <div className="card-actions justify-end">
